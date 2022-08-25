bfe949377a9baff0319370caf160dd08762984ce
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-08-25 23:15:32 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+    const input = document.querySelector('form')
+    input.addEventListener('submit',(e)=>{
+        e.preventDefault()
+       const input = document.querySelector('input#searchByID')
+      //  console.log(input)
+       fetch(`http://localhost:3000/movies/${input.value}`)
+       .then(response => response.json())
+  
+       .then(data => {
+          const title = document.querySelector('section#movieDetails h4');
+          const summary = document.querySelector('section#m ovieDetails p');
+  
+          title.innerText = data.title
+          summary.innerText = data.summary
+  
+  
+       }
+  
+       )
+       })
+  }
+  
+  document.addEventListener('DOMContentLoaded', init)
